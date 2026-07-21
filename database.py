fb33f8f90de9ae438d9be1f9e657f5fe3aa70fd5
abhay_badoni <f20231111@pilani.bits-pilani.ac.in>
2026-07-21 16:46:00 +0530

    Support DATABASE_URL for deployment

--- database.py
+++ database.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
+    db_url = os.getenv("DATABASE_URL")
+    if db_url:
+        return psycopg2.connect(db_url)
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
